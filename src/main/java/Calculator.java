6f2847c878ee78b8a64f555fe777ce3661ef5b45
kimich <ickim1218@gmail.com>
2022-11-17 17:20:28 +0900

    feat: 예시

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Calculator {
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+    static double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b);
+    }
 }
