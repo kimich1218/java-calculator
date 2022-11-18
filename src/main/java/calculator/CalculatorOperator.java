6fac035ce750526b1dbedeaee015c00c99bcdc2d
kimich <ickim1218@gmail.com>
2022-11-18 16:47:20 +0900

    feat: 사칙연산 판별 구현

--- src/main/java/calculator/CalculatorOperator.java
+++ src/main/java/calculator/CalculatorOperator.java
@@ -17,4 +17,42 @@ public class CalculatorOperator {
     public static int divide(int number1, int number2) {
         return number1 / number2;
     }
+
+    public static Boolean distinguishAdd(String operatorAdd) {
+
+        if (operatorAdd.equals("+")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static Boolean distinguishSubtract(String operatorSubract) {
+
+        if (operatorSubract.equals("-")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static Boolean distinguishMultiple(String operatorMultiple) {
+
+        if (operatorMultiple.equals("*")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static Boolean distinguishDivide(String operatorDivide) {
+
+        if (operatorDivide.equals("/")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
