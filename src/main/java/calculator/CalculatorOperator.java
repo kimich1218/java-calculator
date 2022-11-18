26314104e9f9628552022e1b08215867e167102b
kimich <ickim1218@gmail.com>
2022-11-18 12:40:42 +0900

    feat: 사칙연산 기능 구현

---
+++ src/main/java/calculator/CalculatorOperator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class CalculatorOperator {
+
+    public static int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public static int multiple(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public static int divide(int number1, int number2) {
+        return number1 / number2;
+    }
+}
