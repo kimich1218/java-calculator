769c5425984e20a8af5e3f74542660d7988fbd5e
kimich <ickim1218@gmail.com>
2022-11-18 12:39:05 +0900

    feat: 최종 실행 기능 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import calculator.CalculatorRun;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        CalculatorRun calculatorRun = new CalculatorRun();
+        calculatorRun.run();
+    }
+}
